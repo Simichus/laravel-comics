36070949b8aac98570051676b3621cd1ef41d7a5
Simichus <simonefilippobilardo@gmail.com>
2021-10-01 15:02:24 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <div>DC POWER&trade; VISA&reg;</div>
+            <div>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></div>
+        </div>
+    </div>
+    <div class="header-nav container">
+        <img src="{{asset('images/dc-logo.png')}}" alt="DC comics">
+        <nav>
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP <i class="fas fa-caret-down"></i></a></li>
+            </ul>
+        </nav>
+        <div class="search">
+            <input type="text" id="search" placeholder="Search...">
+            <i class="fas fa-search"></i>
+        </div>
+    </div>
+    <div class="header-jumbo">
+    </div>
+</header>
